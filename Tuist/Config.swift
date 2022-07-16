cf1ad8ac6bd84d2d18e111a160bbd433e0d26f16
Andrew Lord <andrew@lordcodes.com>
2022-07-16 17:29:34 +0100

    Run SwiftLint on the project using Tuist

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,6 +2,7 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
-        .git(url: "https://github.com/lordcodes/swiftformat-tuist", tag: "v0.2.0"),
+        .git(url: "https://github.com/lordcodes/swiftformat-tuist", tag: "v0.3.0"),
+        .git(url: "https://github.com/lordcodes/swiftlint-tuist", tag: "v0.1.0"),
     ]
 )
